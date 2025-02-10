81131bd6b345c13e37afd4c80d0fb4ae0ca64b5c
robainggg <robincastel2003@gmail.com>
2025-02-10 10:22:47 +0100

    Initial commit

---
+++ src/main/java/iut/sae/saeconnectback/SaeconnectbackApplication.java
@@ -0,0 +1,13 @@
+package iut.sae.saeconnectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaeconnectbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaeconnectbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iut/sae/saeconnectback/SaeconnectbackApplicationTests.java
@@ -0,0 +1,13 @@
+package iut.sae.saeconnectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaeconnectbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
