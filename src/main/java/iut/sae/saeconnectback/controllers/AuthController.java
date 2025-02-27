77d967f0ce23e65f0b0946915a5d04a688bbf5bf
rcastel <robincastel2003@gmail.com>
2025-02-27 10:56:58 +0100

    Authentification fonctionnelle

--- src/main/java/iut/sae/saeconnectback/controllers/AuthController.java
+++ src/main/java/iut/sae/saeconnectback/controllers/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -59,6 +60,7 @@ public class AuthController {
             String token = JwtTokenUtil.generateToken(user.getAlias());
             return ResponseEntity.ok(new JwtResponse(token));
         } catch(Exception ex) {
+            ex.printStackTrace();
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Alias ou mot de passe invalide");
         }
     }
--- src/main/java/iut/sae/saeconnectback/utils/JwtTokenUtil.java
+++ src/main/java/iut/sae/saeconnectback/utils/JwtTokenUtil.java
@@ -2,23 +2,39 @@ package iut.sae.saeconnectback.utils;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
+@Component
 public class JwtTokenUtil {
 
-    @Value("${jwt.secret}")
-    private static String SECRET_KEY;  // Change ça par une clé secrète plus sécurisée
+    private static String SECRET_KEY;
 
-    // Méthode pour générer un token JWT
+    private static SecretKey KEY;
+
+    @Value("${jwt.secret}")
+    public void setSecret(String secret) {
+        SECRET_KEY = secret;
+        KEY = Keys.hmacShaKeyFor(Base64.getEncoder().encode(SECRET_KEY.getBytes()));
+    }
+
+
     public static String generateToken(String alias) {
         return Jwts.builder()
                 .setSubject(alias)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 3600000))  // Token valable 1 heure
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(KEY, SignatureAlgorithm.HS256)
                 .compact();
     }
+
 
 }
